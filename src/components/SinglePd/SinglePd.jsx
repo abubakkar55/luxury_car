8a4e023cdbd78db1b647a42eaf6bacee5eda6f51
Ab <computerm246@gmail.com>
2021-11-22 21:51:45 +0600

    show animation on  add-to-cart

--- src/components/SinglePd/SinglePd.jsx
+++ src/components/SinglePd/SinglePd.jsx
@@ -9,7 +9,7 @@ const SinglePd = ({ name, image, description, _id, price, condition, fuel }) => 
     return (
         <div data-aos="zoom-in" data-aos-delay="200" data-aos-duration="600" className="car__card p-5 shadow rounded-md">
             <div className="relative">
-                <div>
+                <div className="card__content">
                     <button className="px-4 py-1 text-sm outline-none  bg-orange-500 rounded shadow text-white absolute left-0">{condition} </button>
                     <button
                         className="px-4 py-1 text-sm font-semibold outline-none  border-2 border-orange-500 rounded 
@@ -20,11 +20,11 @@ const SinglePd = ({ name, image, description, _id, price, condition, fuel }) => 
                     <img className="mx-auto pt-10 w-64 h-48" src={`data:image/png;base64,${image}`} alt="car-img" />
                 </div>
                 <div>
-                    <h3 className="text-xl font-semibold mb-3">{name} </h3>
-                    <p className="mb-3 text-gray-500 leading-tight">{description?.slice(0, 110)} </p>
-                    <div className="flex flex-col md:flex-row justify-between items-stretch md:items-center">
+                    <h3 className="card__content text-xl font-semibold mb-3">{name} </h3>
+                    <p className="card__content mb-3 text-gray-500 leading-tight">{description?.slice(0, 110)} </p>
+                    <div className="add__to__card flex flex-col md:flex-row justify-between items-stretch md:items-center">
                         <button className="text-2xl font-semibold mb-3 sm:mb-0  outline-none"> ${price} </button>
-                        <button className="add__to__card px-4 py-2 font-semibold  outline-none  bg-orange-500 rounded shadow text-white"> <NavLink to={`products_details/${_id}`}>
+                        <button className="px-4 py-2 font-semibold  outline-none  bg-orange-500 rounded shadow text-white"> <NavLink to={`products_details/${_id}`}>
                             <FontAwesomeIcon className="mr-1" icon={faShoppingCart} />
                             Buy Now </NavLink> </button>
                     </div>
