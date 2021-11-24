377330677252ab374bdde842e8584b0e6689c7e2
Ab <computerm246@gmail.com>
2021-11-24 23:43:17 +0600

    added  file

--- src/components/SinglePd/SinglePd.jsx
+++ src/components/SinglePd/SinglePd.jsx
@@ -6,6 +6,7 @@ import { faGasPump, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const SinglePd = ({ name, image, description, _id, price, condition, fuel }) => {
 
+    
     return (
         <div data-aos="zoom-in" data-aos-delay="200" data-aos-duration="600" className="car__card p-5 shadow rounded-md">
             <div className="relative">
@@ -21,7 +22,7 @@ const SinglePd = ({ name, image, description, _id, price, condition, fuel }) => 
                 </div>
                 <div>
                     <h3 className="card__content text-xl font-semibold mb-3">{name} </h3>
-                    <p className="card__content mb-3 text-gray-500 leading-tight">{description?.slice(0, 110)} </p>
+                    <p className="card__content card--content--details   text-gray-500 leading-tight">{description?.slice(0, 110)} </p>
                     <div className="add__to__card flex flex-col md:flex-row justify-between items-stretch md:items-center">
                         <button className="text-2xl font-semibold mb-3 sm:mb-0  outline-none"> ${price} </button>
                         <button className="px-4 py-2 font-semibold  outline-none  bg-orange-500 rounded shadow text-white"> <NavLink to={`products_details/${_id}`}>
